446aa0791e606a4b7ccf6c7a4f99a08eee1d8dc9
prakasha20 <prakash.a0497@gmail.com>
2017-06-21 14:53:15 +0530

    Create leaf.java

---
+++ leaf.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class leaf
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+if((a%4)==0)
+System.out.println("LEAF YEAR");
+else
+System.out.println("NOT A LEAF YEAR");
+}}
