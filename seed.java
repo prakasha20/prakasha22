6b4bbfa7fd6b97e3afddd971daab4c08c7f0dab0
prakasha20 <prakash.a0497@gmail.com>
2017-07-20 09:17:07 +0530

    Update seed.java

--- seed.java
+++ seed.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class seed {
 	public static void main(String[] arg)
 	{
+		int cout=0;
 	Scanner sc=new Scanner(System.in);
 	int n=sc.nextInt();
 	for(int i=0;i<n;i++)
@@ -17,8 +18,12 @@ public class seed {
 		if(r==n)
 		{
 			System.out.println("seed"+" "+i);
+			count++;
 		}
+		
 	}
+		if(count==0)
+			System.out.println("No seeds");
 	}
 
 }
